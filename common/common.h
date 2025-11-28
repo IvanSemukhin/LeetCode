5390127fbcd60d1e431d0aac76c88e6c099819ff
Ivan Semukhin <ivan-semukhin@yandex.ru>
2025-11-28 14:30:28 +0500

    upd common

--- common/common.h
+++ common/common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 //------------------------------------------------------------------------------------
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <set>
